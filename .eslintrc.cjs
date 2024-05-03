b892b87d054afd13ada19b5fb0751f742a6d9b85
Gary Cason <garrettcason@skiff.com>
2024-05-03 15:52:04 -0500

    mvp build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -8,14 +11,26 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: '18.2'
+    }
+  },
+  plugins: [
+    'react-refresh'
+  ],
   rules: {
     'react/jsx-no-target-blank': 'off',
+    // Disable prop-types rule:
+    'react/prop-types': 0,
+    // Configuration for react-refresh plugin
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
+      { allowConstantExport: true }
+    ]
   },
 }
--- src/services/AllDrinksService.jsx
+++
@@ -1,4 +0,0 @@
-//AllDrinksService.jsx
-export const getAllDrinks = () => {
-    return fetch(`http://localhost:8088/drinks`).then((res) => res.json())
-}
